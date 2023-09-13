86f2f25d8bed62c7a165489dcbdc070ac2f68b32
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-09-14 10:14:08 +1200

    add login functionality

--- client/components/UserDashboard/UserDashboard.tsx
+++ client/components/UserDashboard/UserDashboard.tsx
@@ -1,31 +1,37 @@
 import { Link } from 'react-router-dom'
 import AddCrew from '../AddCrew/AddCrew.js'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function UserDashboard() {
   // TODO: call the useAuth0 hook and destructure getAccessTokenSilently
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
 
   //TODO: Read the database and map through it to display all Crews from user
 
   return (
     <>
-      <ul>
-        {' '}
-        MyUserName&lsquo;s Crews
-        <Link to={'/crew-dashboard'}>
-          <li>Skydiving</li>
-        </Link>
-        <Link to={'/crew-dashboard'}>
-          {' '}
-          <li>Robot Poetry</li>{' '}
-        </Link>
-        <Link to={'/crew-dashboard'}>
-          <li>Cheeseburger</li>
-        </Link>
-        <Link to={'/crew-dashboard'}>
-          <li>Alien Fiesta</li>
-        </Link>
-      </ul>
-      <AddCrew />
+      {isAuthenticated && (
+        <>
+          <ul>
+            {' '}
+            MyUserName&lsquo;s Crews
+            <Link to={'/crew-dashboard'}>
+              <li>Skydiving</li>
+            </Link>
+            <Link to={'/crew-dashboard'}>
+              {' '}
+              <li>Robot Poetry</li>{' '}
+            </Link>
+            <Link to={'/crew-dashboard'}>
+              <li>Cheeseburger</li>
+            </Link>
+            <Link to={'/crew-dashboard'}>
+              <li>Alien Fiesta</li>
+            </Link>
+          </ul>
+          <AddCrew />
+        </>
+      )}
     </>
   )
 }
