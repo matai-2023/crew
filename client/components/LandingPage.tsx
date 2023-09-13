caad61f16a5f45bc0558eadfac05c8f8fe6a8668
Laura Harris <136272064+laura-ellen-harris@users.noreply.github.com>
2023-09-14 11:16:51 +1200

    styling

--- client/components/LandingPage.tsx
+++ client/components/LandingPage.tsx
@@ -1,13 +1,16 @@
 import LoginButton from './LoginButton/LoginButton'
 import RegisterButton from './RegisterButton/RegisterButton'
+import Background from './UI/Background/Background'
 
 function LandingPage() {
   return (
     <>
-      {/* TODO: Include the header with logo  */}
-      <h1>Welcome to CREW</h1>
-      <LoginButton />
-      <RegisterButton />
+      <Background>
+        {/* TODO: Include the header with logo  */}
+        <h1>Welcome to CREW</h1>
+        <LoginButton />
+        <RegisterButton />
+      </Background>
     </>
   )
 }
--- client/components/LoginButton/LoginButton.tsx
+++ client/components/LoginButton/LoginButton.tsx
@@ -1,8 +1,10 @@
+import Button from '../UI/Button/Button'
+
 function LoginButton() {
   // TODO: Import Auth0 and follow rcmndr code
   // TODO: Create a handleSubmit function to make the button clickable
 
-  return <button>Log in</button>
+  return <Button>Log in</Button>
 }
 
 export default LoginButton
