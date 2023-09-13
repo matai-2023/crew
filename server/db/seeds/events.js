b9415a3aee7d9551f49b7de631c5ee7a5e3d6bb5
Sharon-Li <136324075+Sharon-Li-77@users.noreply.github.com>
2023-09-13 14:30:46 +1200

    need crews table to run seed

---
+++ server/db/seeds/events.js
@@ -0,0 +1,10 @@
+
+// export async function seed (knex) {
+//   // Deletes ALL existing entries
+//   await knex('event').del()
+//   await knex('event').insert([
+//     {id: 1, name: 'Dessert Banquet', time:'', location:'12 Morhan st, New Market', description:'', date:'3 Oct', img:'', },
+//     {id: 1, name: 'Scorpio Party', time:'', location:'Cordis Hotel', description:'', date:'20 Nov', img:'' },
+//     {id: 1, name: 'Beach Burn', time:'', location:'Mission Bay', description:'', date:'27 Dec', img:'' },
+//   ]);
+// }
