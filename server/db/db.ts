0fb0135fe05847a4fe8aeb847f0e8fc705619383
Sharon-Li <136324075+Sharon-Li-77@users.noreply.github.com>
2023-09-14 16:11:40 +1200

    get-crew-list backend done

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,6 +1,9 @@
 import db from './connection.js'
 
-export  async function getCrewList() {
-  return db('crews').select()
-  
+export async function getCrewList(auth0id: string) {
+  return db('crew_users')
+    .join('crews', 'crews.id', 'crew_users.crew_id')
+    .join('users', 'users.id', 'crew_users.user_id')
+    .where('auth0id', auth0id)
+    .select('crews.name', 'crews.image')
 }
--- server/routes/crews.ts
+++ server/routes/crews.ts
@@ -3,12 +3,16 @@ import * as db from '../db/db.ts'
 
 const router = express.Router()
 
-router.get('/', async(req,res)=>{
-  try{
-    const crews= await db.getCrewList()
+router.get('/', async (req, res) => {
+  try {
+    //  Todo: replace the hardcode Auth0 with Auth0 function
+    const auth0 = 'auth0|6502325ffee50bd6057c4e09'
+    const crews = await db.getCrewList(auth0)
     res.json(crews)
-  } catch(err){
+  } catch (err) {
     console.log(err)
     res.status(500).send('Could not find crew list')
   }
 })
+
+export default router
