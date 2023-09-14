58ad964f78d4b16d52923a71d64d827eb421c138
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-09-15 10:15:22 +1200

    render diff header based on authentication

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
-
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import LandingPage from './LandingPage.tsx'
 import UserDashboard from './UserDashboard/UserDashboard.tsx'
 import CrewDashboard from './CrewDashboard/CrewDashboard.tsx'
 import EventDetails from './EventDetails/EventDetails.tsx'
 import NewEvent from './NewEvent/NewEvent.tsx'
+import Header from './UI/Header/Header.tsx'
+import DashboardHeader from './UI/DashboardHeader/DashboardHeader.tsx'
 
 function App() {
   //TODO: Create the routes to UserDashboard, CrewDashboard, EventDetails, etc
   return (
     <>
+      <IfAuthenticated>
+        <DashboardHeader />
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <Header />
+      </IfNotAuthenticated>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/user-dashboard" element={<UserDashboard />} />
