a53f20864047d813dda0b77715c7f409c817757e
Laura Harris <136272064+laura-ellen-harris@users.noreply.github.com>
2023-09-14 15:38:49 +1200

    background and header added to main pages/components

--- client/components/NewEvent/NewEvent.tsx
+++ client/components/NewEvent/NewEvent.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import Background from '../UI/Background/Background'
+import DashboardHeader from '../UI/DashboardHeader/DashBoardHeader'
 
 function NewEvent() {
   //TODO: Create a function to handle the date selection
@@ -6,23 +8,26 @@ function NewEvent() {
   //TODO: Create a function to handle the Add Event form
   return (
     <>
-      <form action="submit">
-        <label htmlFor="">
-          <input type="text" placeholder="Event name" required></input>
-        </label>
-        <label htmlFor="">
-          <input type="date" required></input>
-        </label>
-        <label htmlFor="">
-          <input type="text" placeholder="Event location" required></input>
-        </label>
-        <label htmlFor="">
-          <input type="text" placeholder="Event Details" required></input>
-        </label>
+      <Background>
+        <DashboardHeader />
+        <form action="submit">
+          <label htmlFor="">
+            <input type="text" placeholder="Event name" required></input>
+          </label>
+          <label htmlFor="">
+            <input type="date" required></input>
+          </label>
+          <label htmlFor="">
+            <input type="text" placeholder="Event location" required></input>
+          </label>
+          <label htmlFor="">
+            <input type="text" placeholder="Event Details" required></input>
+          </label>
 
-        <button>Add Event</button>
-      </form>
-      <Link to={'/crew-dashboard'}>Cancel</Link>
+          <button>Add Event</button>
+        </form>
+        <Link to={'/crew-dashboard'}>Cancel</Link>
+      </Background>
     </>
   )
 }
