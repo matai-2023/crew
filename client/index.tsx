b15298e745efcc6c2281d26fd4abee02c9f25c3b
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-09-15 16:30:51 +1200

    remove slash

--- client/index.tsx
+++ client/index.tsx
@@ -1,19 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
-import {
-  Outlet,
-  Route,
-  BrowserRouter as Router,
-  RouterProvider,
-  createMemoryRouter,
-  createRoutesFromElements,
-} from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
 import routes from './components/routes.tsx'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
+  const queryClient = new QueryClient()
   createRoot(document.getElementById('app') as HTMLElement).render(
     /**
      * Auth0Provider is a component that has a hook that provides
@@ -22,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <Auth0Provider
       domain="matai-2023-hannah.au.auth0.com"
       clientId="v55PJUDa1Pqu5HddSg4jnRCQBpd61alR"
-      redirectUri={`${window.location.origin}/`}
+      redirectUri={`${window.location.origin}`}
       audience="https://crew/api"
       cacheLocation="localstorage"
     >
