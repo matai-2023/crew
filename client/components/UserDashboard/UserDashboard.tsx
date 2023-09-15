656c8e5200590ab16f78b79f3b8507b59eefc6ce
Sharon-Li <136324075+Sharon-Li-77@users.noreply.github.com>
2023-09-15 17:39:33 +1200

    latest update-test to be done after auth0

--- client/components/UserDashboard/UserDashboard.tsx
+++ client/components/UserDashboard/UserDashboard.tsx
@@ -4,23 +4,47 @@ import { useQuery } from '@tanstack/react-query'
 import { getCrewList } from '../../../server/db/db.js'
 import { useAuth0 } from '@auth0/auth0-react'
 import { fetchCrewList } from '../../apis/crews.js'
+import { useEffect, useState } from 'react'
 
 function UserDashboard() {
-  // const { id } = useAuth0()
-  // console.log(id)
-
-  const { data } = useQuery(['crews'], fetchCrewList)
-  console.log(data)
   // TODO: call the useAuth0 hook and destructure getAccessTokenSilently
-
   //TODO: Read the database and map through it to display all Crews from user
   // Note: 'ADD CREW' is just a button for MVP
 
+  // const { id } = useAuth0()
+  // console.log(id)
+
+  const { data, isLoading } = useQuery(['crews'], fetchCrewList)
+  const [uniqueName, setUniqueName] = useState([] as crews[])
+
+  useEffect(() => {
+    if (!isLoading) {
+      setUniqueName(filterDuplicatedName(data))
+    }
+  }, [data, isLoading])
+
+  interface crews {
+    name: string
+    image: string
+  }
+
+  function filterDuplicatedName(array: crews[]) {
+    // const mySet = new Set()
+    return array
+      ? array.reduce((acc: crews[], p: crews) => {
+          return acc.find((obj: crews) => obj.name == p.name)
+            ? acc
+            : [...acc, p]
+        }, [])
+      : []
+  }
+
   return (
     <>
+      <p>{isLoading ? 'please wait' : ''}</p>
       <ul>
-        {data &&
-          data.map((p, i) => (
+        {uniqueName &&
+          uniqueName.map((p, i) => (
             <li key={i}>
               <h1>{p.name}</h1>
               <img src={p.image} alt={p.name} />
