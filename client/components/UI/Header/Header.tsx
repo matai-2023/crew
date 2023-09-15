6dbcd90b7a6b846d5e07b083a223b0d3a67a9ea7
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-09-15 18:21:27 +1200

    move logo back into link

--- client/components/UI/Header/Header.tsx
+++ client/components/UI/Header/Header.tsx
@@ -4,8 +4,8 @@ import Logo from '../Logo/Logo.tsx'
 function Header() {
   return (
     <div className="pl-4 pt-3 pr-4 flex justify-between items-center">
-      <Link to="/" className="absolute top-0 right-0 mt-3 mr-4"></Link>
-      <Logo />
+      <Link to="/" className="absolute top-0 right-0 mt-3 mr-4"><Logo /></Link>
+      
     </div>
   )
 }
