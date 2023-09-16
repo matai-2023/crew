f0a709b54e02109c13b50af7b5ff84859604b72a
Laura Harris <136272064+laura-ellen-harris@users.noreply.github.com>
2023-09-16 13:06:44 +1200

    header componenet finished on landing page

--- client/components/LandingPage.tsx
+++ client/components/LandingPage.tsx
@@ -18,6 +18,7 @@ function LandingPage() {
   return (
     <>
       <Background>
+        <Header />
         <div className="pt-44 pl-4 flex flex-col gap-4">
           {titles.map((title, i) => (
             <motion.div
