1c6cd499ad0e23f5486c88e70a0dc360c3bae400
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-09-20 14:33:05 +1200

    fix syntax issues

--- client/components/NewEvent/NewEvent.tsx
+++ client/components/NewEvent/NewEvent.tsx
@@ -9,81 +9,64 @@ function NewEvent() {
   const navigate = useNavigate()
 
   return (
-    <>
-      <div className="h-screen max-h-[calc(100vh-64px)] overflow-y-auto">
-        <form action="submit">
-          <div className="mb-4 p-3">
-            <label
-              htmlFor="eventName"
-              className="block text-white font-interReg"
-            >
-              Event name
-            </label>
-            <input
-              type="text"
-              id="eventName"
-              className="border rounded-md w-full px-3 py-2 text-black"
-              required
-            />
-          </div>
-          <div className="mb-4 p-3">
-            <label
-              htmlFor="eventDate"
-              className="block text-white font-interReg"
-            >
-              Event date
-            </label>
-            <input
-              type="date"
-              id="eventDate"
-              className="border rounded-md w-full px-3 py-2 text-black"
-              required
-            />
-          </div>
-          <div className="mb-4 p-3">
-            <label
-              htmlFor="eventLocation"
-              className="block text-white font-interReg"
-            >
-              Event location
-            </label>
-            <input
-              type="text"
-              id="eventLocation"
-              className="border rounded-md w-full px-3 py-2 text-black"
-              required
-            />
-          </div>
-          <div className="mb-4 p-3">
-            <label
-              htmlFor="eventDetails"
-              className="block text-white font-interReg"
-            >
-              Event Details
-            </label>
-            <input
-              type="text"
-              id="eventDetails"
-              className="border rounded-md w-full px-3 py-2 text-black h-32"
-              required
-            />
-          </div>
-        </form>
-        <div className="mt-4 flex flex-col space-y-4 p-9">
-          <Button>Create Event</Button>
-          <Link
-            to={'/crew-dashboard'}
-            className="text-center text-white font-interReg"
-          >
-            Cancel
-          </Link>
+    <div className="h-screen max-h-[calc(100vh-64px)] overflow-y-auto">
+      <form action="submit">
+        <div className="mb-4 p-3">
+          <label htmlFor="eventName" className="block text-white font-interReg">
+            Event name
+          </label>
+          <input
+            type="text"
+            id="eventName"
+            className="border rounded-md w-full px-3 py-2 text-black"
+            required
+          />
         </div>
-        <div className="mt-4 flex flex-col space-y-4 p-9">
-          <Button onClick={() => navigate('/crew-dashboard')}>
-            Create Event
-          </Button>
+        <div className="mb-4 p-3">
+          <label htmlFor="eventDate" className="block text-white font-interReg">
+            Event date
+          </label>
+          <input
+            type="date"
+            id="eventDate"
+            className="border rounded-md w-full px-3 py-2 text-black"
+            required
+          />
+        </div>
+        <div className="mb-4 p-3">
+          <label
+            htmlFor="eventLocation"
+            className="block text-white font-interReg"
+          >
+            Event location
+          </label>
+          <input
+            type="text"
+            id="eventLocation"
+            className="border rounded-md w-full px-3 py-2 text-black"
+            required
+          />
+        </div>
+        <div className="mb-4 p-3">
+          <label
+            htmlFor="eventDetails"
+            className="block text-white font-interReg"
+          >
+            Event Details
+          </label>
+          <input
+            type="text"
+            id="eventDetails"
+            className="border rounded-md w-full px-3 py-2 text-black h-32"
+            required
+          />
         </div>
       </form>
+      <div className="mt-4 flex flex-col space-y-4 p-9">
+        <Button onClick={() => navigate('/crew-dashboard')}>
+          Create Event
+        </Button>
+      </div>
       <div className="mt-4 flex flex-col space-y-4 ">
         <Link
           to={'/user-dashboard'}
@@ -92,7 +75,7 @@ function NewEvent() {
           Cancel
         </Link>
       </div>
-    </>
+    </div>
   )
 }
 
